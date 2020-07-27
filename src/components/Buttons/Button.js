c8f05a4174d597293db55dbfd80b4f9bbc3adf80
demkantor <demkantor@gmail.com>
2020-07-27 14:06:37 -0500

    set layout for buttons page, added views of options

--- src/components/Buttons/Button.js
+++ src/components/Buttons/Button.js
@@ -1,9 +1,18 @@
 import React from 'react';
 import './Button.css';
 
-const Button = ({ color, icon, size, text, variant  }) => {
+const Button = ({ color, disabled, icon_back, icon_front, size, text, variant  }) => {
     return (
-        <button className={`btn ${color} ${size} ${variant}`}>{ icon } { text }</button>
+        <button 
+            className={`btn ${color} ${size} ${variant}`} disabled={false || disabled}>
+                <span className="material-icons">
+                    { icon_front }
+                </span> 
+                { text }
+                <span className="material-icons">
+                    { icon_back }
+                </span> 
+            </button>
     );
 };
 
--- src/components/Buttons/ButtonsPage.js
+++ src/components/Buttons/ButtonsPage.js
@@ -7,34 +7,97 @@ const ButtonsPage = () => {
         <div className="btns-wrapper">
             <h1 className="title">Buttons Page</h1>
             <section>
+                <div className="section-title">Colors</div>
                 <div className="btn-container">
                     <Button 
-                        text={'hello'}
+                        text={'default'}
+                         />
+                    <Button 
+                        text={'primary'}
+                        color={'primary'} 
+                        />
+                    <Button 
+                        text={'secondary'}
+                        color={'secondary'}  
+                        />
+                    <Button 
+                        text={'success'}
+                        color={'success'}  
+                        />
+                    <Button 
+                        text={'danger'}
+                        color={'danger'}  
+                        />
+                    <Button 
+                        text={'warning'}
+                        color={'warning'}  
+                        />
+                    <Button 
+                        text={'info'}
+                        color={'info'}  
+                        />
+                    <Button 
+                        text={'light'}
+                        color={'light'}  
+                        />
+                    <Button 
+                        text={'dark'}
+                        color={'dark'}  
+                        />
+                </div>
+                <div className="section-title">Sizes</div>
+                <div className="btn-container-lg">
+                    <Button 
+                        text={'small'}
                         color={'primary'} 
                         size={'sm'} />
                     <Button 
-                        text={'hello'}
+                        text={'default'}
                         color={'primary'}  
                         size={'md'} />
                     <Button 
-                        text={'hello'}
+                        text={'large'}
                         color={'primary'} 
                         size={'lg'} />
+                </div>
+                <div>
                     <Button 
-                        value={'hello'} />
-                    <Button>This one</Button>
+                        text={'block level'}
+                        color={'primary'} 
+                        size={'block'} />
                 </div>
-                <div className="container">
-                    
-                </div>
-                <div className="container">
+                <div className="section-title">Variants</div>
+                <div className="btn-container">
                     <Button 
-                        text={'hello'} />
+                        text={'hello'} 
+                        variant={'outline'}/>
                 </div>
-                <div className="container">
+                <div className="section-title">Icons</div>
+                <div className="btn-container-lg">
                     <Button 
-                        text={'hello'} />
+                        text={'icon in front'}
+                        color={'primary'}
+                        size={'lg'}
+                        icon_front={'shopping_cart'} />
+                    <Button 
+                        text={'icon in back'}
+                        color={'primary'}
+                        size={'lg'}
+                        icon_back={'shopping_cart'} />
                 </div>
+                <div className="section-title">Legend</div>
+                <p>
+                    {"text={''}"}
+                    {" options: any text you want to display"}
+                </p>
+                <p>
+                    {"color={''}"}
+                    {" options: primary, secondary, success, danger, "}
+                </p>
+                <p>
+                    {"size={''}"}
+                    {" options: sm, md, lg, block "}
+                </p>
             </section>
         </div>
     );
