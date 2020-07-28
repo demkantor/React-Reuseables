b6cba1cb8007f7845bbe6e2b016935577bdf42e1
demkantor <demkantor@gmail.com>
2020-07-28 17:17:06 -0500

    fixed footer and finished buttons page

--- src/components/Buttons/Button.js
+++ src/components/Buttons/Button.js
@@ -1,11 +1,28 @@
 import React from 'react';
 import './Button.css';
 
-const Button = ({ color, disabled, icon_back, icon_front, onClick, size, text, variant  }) => {
+const Button = ({ 
+    color, 
+    disabled, 
+    icon_back, 
+    icon_front,
+    name,
+    onClick, 
+    size, 
+    text,
+    type,
+    value,
+    variant  
+}) => {
+
     return (
         <button 
-            className={`btn ${color} ${size} ${variant}`} disabled={false || disabled}
-            onClick={onClick}>
+            className={`btn ${color} ${size} ${variant}`} 
+            disabled={false || disabled}
+            name={name}
+            onClick={onClick}
+            type={type}
+            value={value}>
                 <span className="material-icons">
                     { icon_front }
                 </span> 
--- src/components/Buttons/ButtonsPage.js
+++ src/components/Buttons/ButtonsPage.js
@@ -4,10 +4,6 @@ import './Button.css';
 
 const ButtonsPage = () => {
 
-    const btnInfo = (event) => {
-        console.log('hey', event.target)
-    }
-
     return (
         <div className="btns-wrapper">
             <h1 className="title">Buttons Page</h1>
@@ -16,39 +12,47 @@ const ButtonsPage = () => {
                 <div className="btn-container">
                     <Button 
                         text={'default'}
-                        onClick={btnInfo}
+                        onClick={()=>alert( "<Button text={'default'} />" )}
                          />
                     <Button 
                         text={'primary'}
                         color={'primary'} 
+                        onClick={()=>alert( "<Button text={'primary'} color={'primary'}  />" )}
                         />
                     <Button 
                         text={'secondary'}
-                        color={'secondary'}  
+                        color={'secondary'} 
+                        onClick={()=>alert( "<Button text={'secondary'} color={'secondary'}  />" )}
                         />
                     <Button 
                         text={'success'}
-                        color={'success'}  
+                        color={'success'} 
+                        onClick={()=>alert( "<Button text={'success'} color={'success'}  />" )} 
                         />
                     <Button 
                         text={'danger'}
-                        color={'danger'}  
+                        color={'danger'} 
+                        onClick={()=>alert( "<Button text={'danger'} color={'danger'}  />" )} 
                         />
                     <Button 
                         text={'warning'}
                         color={'warning'}  
+                        onClick={()=>alert( "<Button text={'warning'} color={'warning'}  />" )}
                         />
                     <Button 
                         text={'info'}
-                        color={'info'}  
+                        color={'info'} 
+                        onClick={()=>alert( "<Button text={'info'} color={'info'}  />" )} 
                         />
                     <Button 
                         text={'light'}
                         color={'light'}  
+                        onClick={()=>alert( "<Button text={'light'} color={'light'}  />" )}
                         />
                     <Button 
                         text={'dark'}
                         color={'dark'}  
+                        onClick={()=>alert( "<Button text={'dark'} color={'dark'}  />" )}
                         />
                 </div>
                 <div className="section-title">Sizes</div>
@@ -56,27 +60,37 @@ const ButtonsPage = () => {
                     <Button 
                         text={'small'}
                         color={'primary'} 
-                        size={'sm'} />
+                        size={'sm'} 
+                        onClick={()=>alert( "<Button text={'small'} color={'primary'}  size={'sm'} />" )}
+                        />
                     <Button 
                         text={'default'}
                         color={'primary'}  
-                        size={'md'} />
+                        size={'md'} 
+                        onClick={()=>alert( "<Button text={'default'} color={'primary'}  size={'md'} />" )}
+                        />
                     <Button 
                         text={'large'}
                         color={'primary'} 
-                        size={'lg'} />
+                        size={'lg'} 
+                        onClick={()=>alert( "<Button text={'large'} color={'primary'}  size={'lg'} />" )}
+                        />
                 </div>
                 <div>
                     <Button 
                         text={'block level'}
                         color={'primary'} 
-                        size={'block'} />
+                        size={'block'} 
+                        onClick={()=>alert( "<Button text={'block level'} color={'primary'}  size={'block'} />" )}
+                        />
                 </div>
                 <div className="section-title">Variants</div>
                 <div className="btn-container">
                     <Button 
-                        text={'hello'} 
-                        variant={'outline'}/>
+                        text={'outline'} 
+                        variant={'outline'}
+                        onClick={()=>alert( "<Button text={'outline'} variant={'outline'} />" )}
+                        />
                 </div>
                 <div className="section-title">Icons</div>
                 <div className="btn-container-lg">
@@ -84,12 +98,16 @@ const ButtonsPage = () => {
                         text={'icon in front'}
                         color={'primary'}
                         size={'lg'}
-                        icon_front={'shopping_cart'} />
+                        icon_front={'shopping_cart'} 
+                        onClick={()=>alert( "<Button text={'icon in front'} color={'primary'}  size={'lg'} icon_front={'shopping_cart'}  />" )}
+                        />
                     <Button 
                         text={'icon in back'}
                         color={'primary'}
                         size={'lg'}
-                        icon_back={'shopping_cart'} />
+                        icon_back={'shopping_cart'} 
+                        onClick={()=>alert( "<Button text={'icon in back'} color={'primary'}  size={'lg'} icon_back={'shopping_cart'}  />" )}
+                        />
                 </div>
                 <div className="section-title">Legend</div>
                 <p className="legend-block">
@@ -107,6 +125,10 @@ const ButtonsPage = () => {
                 <p className="legend-block">
                     {"variant={''}"}
                     {" options: outline "}
+                </p>
+                <p className="legend-block">
+                    {"icon_back={''} or icon_front={''}"}
+                    {" options: anything in material icons list "}
                 </p>
             </section>
         </div>
